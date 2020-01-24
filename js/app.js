27220f285d2a650620cbf46d9d7feccc021b0df5
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-01-24 18:57:49 +0300

    Edited project files

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@ new Vue({
 		x: 0,
 		y: 0,
 		value: '',
+		name: 'ilteris',
 	},
 	methods: {
 		showAlert : function(name){
 			alert(name);
 		},
 		increaseCounter : function(){
-			return this.counter++;
+			this.counter++;
+		},
+		result : function(){
+			return this.counter > 10 ? "Counter bigger than 10" : "Counter smaller than 10";
 		},
 		updateCoords : function(event){
 			this.x = event.clientX;
@@ -22,6 +26,19 @@ new Vue({
 		},
 		showAlert3 : function(event){
 			this.value = event.target.value;
+		},
+	},
+	computed: {
+		output : function(){
+			return this.counter > 10 ? "Counter bigger than 10" : "Counter smaller than 10";
+		},
+	},
+	watch: {
+		counter : function(value){
+			vm = this;
+			setTimeout(function(){
+				vm.counter = 0;
+			}, 1500);
 		}
-	}
+	},	
 });
