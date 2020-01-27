3dfefd96fe4ef5a68f222c830f8be3955f69ac79
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-01-27 15:19:13 +0300

    Added new folder and files

--- v-for/app.js
+++ v-for/app.js
@@ -1,5 +1,38 @@
 window.addEventListener('load', () => {
+
 	window.vue = new Vue({
-		
-	});
+		el: '#app',
+		name: 'Cart',
+		data: {
+			isLoading: true,
+			cart: [],
+			saved: []
+		},
+		methods: {
+			removeFromCart: function(index){
+				this.cart.splice(index, 1);
+			},
+			removeFromSavedList: function(index){
+				this.saved.splice(index);
+			},
+			saveForLater: function(index){
+				const item = this.cart.splice(index, 1);
+				this.saved.push(item[0]);
+			},
+			moveToCart: function(index){
+				const item = this.saved.splice(index, 1);
+				this.cart.push(item[0]);
+			}
+		},
+
+		created() {
+			fetch('./data.json')
+			.then((res) => { return res.json() })
+			.then((res) => { 
+				this.isLoading = false;
+				this.cart = res.cart;
+				this.saved = res.saved;
+			})
+		}
+	})
 });
